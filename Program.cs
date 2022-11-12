030bb98e62a5314051c738bea8e534ce643a5ac1
Alexandra788 <andeytrof45@gmail.com>
2022-11-12 18:26:52 +0700

    сделана функция которая возвращает количество строк  в массиве которые меньше или равны трем символам

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿int m;
+﻿int lengthArrInput, lengthArrResolt;
 
 string number1;
+
+
 
 Console.Write("Введите желаемое количество слов  ");
 
 number1 = Console.ReadLine()!;
 
-int.TryParse(number1, out m);
+int.TryParse(number1, out lengthArrInput);
 
-string [] array =  new string [m];
+string [] array =  new string [lengthArrInput];
 
 for(int i = 0; i < array.Length; i ++) {
     Console.Write("Введите желаемые слова  ");
@@ -19,3 +21,23 @@ for(int i = 0; i < array.Length; i ++) {
     Console.Write(array[i]);
     
 }
+
+lengthArrResolt = getAmountStringlessFourSimdol(array);
+
+string [] newArray =  new string [lengthArrResolt];
+
+Console.Write(lengthArrResolt);
+
+int getAmountStringlessFourSimdol(string [] _arr) {
+    int count = 0;
+    for(int i = 0; i < _arr.Length; i++) {
+        if(_arr[i].Length < 4) {
+            count++;
+        }
+        
+    }
+
+    return count; 
+
+}
+
