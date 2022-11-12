35207b9ec72e87b9c4e42ead1bc3bfce766ffb06
Alexandra788 <andeytrof45@gmail.com>
2022-11-12 18:05:01 +0700

    реализовали заполнение массива с клавиатуры

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int m;
+
+string number1;
+
+Console.Write("Введите желаемое количество слов  ");
+
+number1 = Console.ReadLine()!;
+
+int.TryParse(number1, out m);
+
+string [] array =  new string [m];
+
+for(int i = 0; i < array.Length; i ++) {
+    Console.Write("Введите желаемые слова  ");
+    array[i] = Console.ReadLine()!;
+}
+
+for(int i = 0; i < array.Length; i ++) {
+    Console.Write(array[i]);
+    
+}
